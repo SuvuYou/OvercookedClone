6f1c81ce6d89eff1d2bf8368739b87f3b1243fe4
SuvuYou <nazarsuvyliak1@gmail.com>
2024-06-11 18:23:01 +0300

    use native random method

--- Assets/Scripts/Helpers/TimingTimer.cs
+++ Assets/Scripts/Helpers/TimingTimer.cs
@@ -1,14 +1,17 @@
-using UnityEngine;
+
+using System;
 
 public struct TimingTimer
 {
     public float Timer { get; private set; }
     private float _minDefaultTimerValue;
     private float _maxDefaultTimerValue;
+    private Random _random;
 
     public TimingTimer (float defaultTimerValue){
         _minDefaultTimerValue = defaultTimerValue;
         _maxDefaultTimerValue = defaultTimerValue;
+        _random = new Random();
 
         Timer = 0f;
         ResetTimer();
@@ -17,6 +20,7 @@ public struct TimingTimer
     public TimingTimer (float minDefaultTimerValue, float maxDefaultTimerValue){
         _minDefaultTimerValue = minDefaultTimerValue;
         _maxDefaultTimerValue = maxDefaultTimerValue;
+        _random = new Random();
 
         Timer = 0f;
         ResetTimer();
@@ -34,6 +38,6 @@ public struct TimingTimer
 
     public float GetRandomTimeInRange()
     {
-        return Random.Range(_minDefaultTimerValue, _maxDefaultTimerValue);
+        return ((float)_random.NextDouble() * (_maxDefaultTimerValue - _minDefaultTimerValue)) + _minDefaultTimerValue;
     }
 }
